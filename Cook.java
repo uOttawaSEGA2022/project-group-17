e0b076728dd30e8a56b3e1989c1662a968387ff4
Sami Hassan <113709975+sami-hssn@users.noreply.github.com>
2022-10-09 17:35:52 -0400

    Add files via upload
    Constructor and instance variables for Cook class

---
+++ Cook.java
@@ -0,0 +1,28 @@
+
+public class Cook{
+
+    private String username;
+    private String password;
+    private String firstName;
+    private String surname;
+    private String email;
+    private int[] bankInfo;
+    private double rating;
+    private String description;
+    private boolean isBanned;
+
+    public Cook(String firstName, String lastName, String username, String password, String email, int[] bankInfo, double rating, String description, boolean isBanned){
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.bankInfo = bankInfo;
+    }
+
+    // getMethods
+
+
+
+}
