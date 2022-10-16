44d9832b732490ec6b4f21985e36f7e0c310e62e
sami-hssn <113709975+sami-hssn@users.noreply.github.com>
2022-10-16 17:57:58 -0400

    constructors, get and set methods

--- app/src/main/java/com/example/kachow_now/Client.java
+++ app/src/main/java/com/example/kachow_now/Client.java
@@ -1,19 +1,81 @@
 package com.example.kachow_now;
 
 public class Client {
-    private String userName;
-    private String firstName;
-    private String lastName;
+    private String username;
+    private String password;
+    private String firstname;
+    private String lastname;
     private String email;
-    private int creditCardNumber;
-    private int expMonth;
-    private int expYear;
-    private int ccv;
+    private int[] bank;
+    private String address;
+    private long phoneNumber;
 
-    public Client(String userName, String firstName, String lastName, String email) {
-        this.userName = userName;
-        this.firstName = firstName;
-        this.lastName = lastName;
+    public Client(String username, String password, String firstname, String lastname, String email, int[] bank, String address, long phoneNumber) {
+        this.username = username;
+        this.password = password;
+        this.firstname = firstname;
+        this.lastname = lastname;
         this.email = email;
+        this.bank = bank;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
     }
+
+    // setter methods
+    public void setFirstName(String newFirstName) {
+        firstname = newFirstName;
+    }
+    public void setLastName(String newLastName){
+        lastname = newLastName;
+    }
+    public void setUserName(String newUserName){
+        username = newUserName;
+    }
+    public void setPassword(String newPassword){
+        password = newPassword;
+    }
+    public void setAddress(String newAddress){
+        address = newAddress;
+    }
+    public void setEmail(String newEmail){
+        email = newEmail;
+    }
+    public void setPhoneNumber(long newNumber){
+        phoneNumber = newNumber;
+    }
+    public void setBank(int creditCardNumber, int expMonth, int expYear, int ccv){
+        int bank[] = new int[4];
+        bank[0] = creditCardNumber;
+        bank[1] = expMonth;
+        bank[2] = expYear;
+        bank[3] = ccv;
+    }
+
+    // getter methods
+    public String getFirstName() {
+        return firstname;
+    }
+    public String getLastName(){
+        return lastname;
+    }
+    public String getUserName(){
+        return username;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public String address(){
+        return address;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public long getPhoneNumber(){
+        return phoneNumber;
+    }
+    public int[] getBank(){
+        return bank;
+    }
+
+
 }
--- app/src/main/java/com/example/kachow_now/Cook.java
+++ app/src/main/java/com/example/kachow_now/Cook.java
@@ -4,24 +4,115 @@ public class Cook{
 
     private String username;
     private String password;
-    private String firstName;
-    private String lastName;
+    private String firstname;
+    private String lastname;
     private String email;
-    private int[] bankInfo;
+    private String address;
+    private int[] bank;
     private double rating;
+    private Meal[] menu;
+    private Meal[] log;
     private String description;
     private boolean isBanned;
 
-    public Cook(String firstName, String lastName, String username, String password, String email, int[] bankInfo, double rating, String description, boolean isBanned){
 
-        this.firstName = firstName;
-        this.lastName = lastName;
+
+    public Cook(String firstname, String lastname, String username, String password, String email, String address, int[] bank, double rating, String description, Meal[] menu, Meal[] log, boolean isBanned){
+
+        this.firstname = firstname;
+        this.lastname = lastname;
         this.username = username;
         this.password = password;
         this.email = email;
-        this.bankInfo = bankInfo;
+        this.address = address;
+        this.bank = bank;
+        this.rating = rating;
+        this.menu = menu;
+        this.log = log;
+        this.description = description;
+        this.isBanned = isBanned;
 
     }
+    // setter methods
+    public void setFirstName(String newFirstName) {
+        firstname = newFirstName;
+    }
+    public void setLastName(String newLastName){
+        lastname = newLastName;
+    }
+    public void setUserName(String newUserName){
+        username = newUserName;
+    }
+    public void setPassword(String newPassword){
+        password = newPassword;
+    }
+    public void setAddress(String newAddress){
+        address = newAddress;
+    }
+    public void setEmail(String newEmail){
+        email = newEmail;
+    }
+    public void setBank(int transit, int institution, int account){
+        int bank[] = new int[3];
+        bank[0] = transit;
+        bank[1] = institution;
+        bank[2] = account;
+    }
+    public void setRating(double newRating){
+        rating = newRating;
+    }
+    // idk abt these
+    public void setLog(){
+    }
+    public void setMenu(){
+    }
+
+    public void setDescription(String newDescription){
+        description = newDescription;
+    }
+    public void setIsBanned(boolean newIsBanned){
+        isBanned = newIsBanned;
+    }
+
+    // getter methods
+    public String getFirstName() {
+        return firstname;
+    }
+    public String getLastName(){
+        return lastname;
+    }
+    public String getUserName(){
+        return username;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public String address(){
+        return address;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public int[] getBank(){
+        return bank;
+    }
+    public double getRating(){
+        return rating;
+    }
+    public Meal[] getLog(){
+        return log;
+    }
+    public Meal[] getMenu(){
+        return menu;
+    }
+    public String getDescription(){
+        return description;
+    }
+    public boolean getIsBanned(){
+        return isBanned;
+    }
+
+
 
     // getMethods
     import { getDatabase } from "firebase/database";
