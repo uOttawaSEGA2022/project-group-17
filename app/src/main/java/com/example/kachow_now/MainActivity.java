5c99daad4b2acb13e1d123a7e8cbb79d39964483
Nathan Le <nle056@uottawa.ca>
2022-10-19 13:06:45 -0400

    Initial commit

--- app/src/main/java/com/example/kachow_now/MainActivity.java
+++ app/src/main/java/com/example/kachow_now/MainActivity.java
@@ -3,8 +3,15 @@ package com.example.kachow_now;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editTextUsername;
+    EditText editTextPassword;
+    DatabaseReference databaseProducts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
