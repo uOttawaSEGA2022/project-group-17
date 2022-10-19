aee9d3077c0bac53c77b47e24245bbc311213cf6
Aaditya Shah <ashah203@uottawa.ca>
2022-10-19 16:45:14 -0400

    edited login methods in firebase

---
+++ app/src/main/java/com/example/kachow_now/welcome.java
@@ -0,0 +1,13 @@
+package com.example.kachow_now;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class welcome extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
