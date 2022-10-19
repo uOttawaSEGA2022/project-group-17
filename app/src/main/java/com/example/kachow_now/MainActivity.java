5cb71478389bc8d2d0885dc6ff60781e1f531fbe
Nathan Le <nle056@uottawa.ca>
2022-10-19 13:37:42 -0400

    Ui

--- app/src/main/java/com/example/kachow_now/MainActivity.java
+++ app/src/main/java/com/example/kachow_now/MainActivity.java
@@ -8,20 +8,32 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText editTextUsername;
     EditText editTextPassword;
     DatabaseReference databaseProducts;
+    Button login;
+    Button signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        databaseProducts = FirebaseDatabase.getInstance().getReference("products");
+        editTextUsername = (EditText) findViewById(R.id.editTextUsername);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+
+    }
+    private void onClickSignup(){
+
+    }
+    private void login(EditText username, EditText password){
+
 
     }
 }
