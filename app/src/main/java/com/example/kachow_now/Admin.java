9046d5714c3f79a30bd6923c15715a3a6ebc8a08
sami-hssn <113709975+sami-hssn@users.noreply.github.com>
2022-10-21 10:19:21 -0400

    Added Admin class.

--- app/src/main/java/com/example/kachow_now/Admin.java
+++ app/src/main/java/com/example/kachow_now/Admin.java
@@ -6,27 +6,33 @@ public class Admin {
     private String password;
     private String nickname;
 
-    public Admin(String email, String password, String nickname){
+    public Admin(String email, String password, String nickname) {
         this.email = email;
         this.password = password;
         this.nickname = nickname;
     }
 
-    public String getEmail(){
+    public String getEmail() {
         return email;
     }
-    public void setEmail(String email){
+
+    public void setEmail(String email) {
         this.email = email;
     }
-    public String getPassword(){
+
+    public String getPassword() {
         return password;
     }
-    public void setPassword(String email){
+
+    public void setPassword(String email) {
         this.password = password;
     }
-    public String getNickname(){
+
+    public String getNickname() {
         return nickname;
     }
-    public void setNickname(String email){
+
+    public void setNickname(String email) {
         this.nickname = nickname;
     }
+}
