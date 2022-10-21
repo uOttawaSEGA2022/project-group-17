6ba1e97f5f091135183b81a90823a10a3c636fe4
Nathan Le <nle056@uottawa.ca>
2022-10-20 23:00:46 -0400

    more buttons on welcome page

--- app/src/main/java/com/example/kachow_now/WelcomePage.java
+++ app/src/main/java/com/example/kachow_now/WelcomePage.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -19,7 +20,8 @@ public class WelcomePage extends AppCompatActivity {
 
         TextView Name = (TextView) findViewById(R.id.clientname);
         TextView Role = (TextView) findViewById(R.id.textView7);
-
+        Button logOutButton = (Button) findViewById(R.id.logoutButton);//not very useful but will link anyway
+        Button continueButton = (Button) findViewById(R.id.Continue);
 
         @Override
         protected void onCreate(Bundle savedInstanceState){
