b9d535e65734a418f590577b4eeb16f0a31ad2fe
Nathan Le <nle056@uottawa.ca>
2022-10-21 10:09:48 -0400

    Admin activity

--- app/src/main/java/com/example/kachow_now/AdminPage.java
+++ app/src/main/java/com/example/kachow_now/AdminPage.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Admin extends AppCompatActivity {
+public class AdminPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin);
+        setContentView(R.layout.activity_adminPage);
     }
 }
