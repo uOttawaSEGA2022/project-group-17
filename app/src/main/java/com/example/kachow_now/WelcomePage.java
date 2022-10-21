d44e0b38e92a8c59d4b02524e3382d7fec82d4da
Nathan Le <nle056@uottawa.ca>
2022-10-20 23:43:35 -0400

    more buttons on welcome page

--- app/src/main/java/com/example/kachow_now/WelcomePage.java
+++ app/src/main/java/com/example/kachow_now/WelcomePage.java
@@ -10,6 +10,11 @@ import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import org.w3c.dom.Text;
 
