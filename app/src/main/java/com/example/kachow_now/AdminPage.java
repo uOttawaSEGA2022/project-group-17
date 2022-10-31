336e857956163bf10dac692ff30638db42b6d7ea
Nathan Le <nle056@uottawa.ca>
2022-10-31 17:53:02 -0400

    AdminPage
    Logout Button functionality

--- app/src/main/java/com/example/kachow_now/AdminPage.java
+++ app/src/main/java/com/example/kachow_now/AdminPage.java
@@ -3,12 +3,33 @@ package com.example.kachow_now;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 
 public class AdminPage extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference dB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adminpage);
+        Button adminLogoutButton = (Button) findViewById(R.id.adminLogoutButton);
+
+
+        adminLogoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                logOut(view);
+            }
+        });
+    }
+    public void logOut(View view){
+        mAuth.signOut();
+        finish();
     }
 }
