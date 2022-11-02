6383e5e3774a5d7dea9cf6f974af8da4dbb20e99
Nathan Le <nle056@uottawa.ca>
2022-11-02 19:30:45 -0400

    updated add meal page (change meal photo is translated 10dp

--- app/src/main/java/com/example/kachow_now/CookProfileClientSide.java
+++ app/src/main/java/com/example/kachow_now/CookProfileClientSide.java
@@ -3,6 +3,8 @@ package com.example.kachow_now;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CookProfileClientSide extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class CookProfileClientSide extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cook_profile_client_side);
+
+        Button reportButton = (Button) findViewById(R.id.ReportCookProfileClient);
+        reportButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TO DO //
+                //setContentView(R.layout.activity_submit_report);
+            }
+        });
+
     }
+
+
 }
