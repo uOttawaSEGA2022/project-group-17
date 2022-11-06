802847c73c1abb22424eaf53d1f917e21b1cb3a8
Nathan Le <nle056@uottawa.ca>
2022-11-06 18:01:18 -0500

    ComplaintList

--- app/src/main/java/com/example/kachow_now/AdminPage.java
+++ app/src/main/java/com/example/kachow_now/AdminPage.java
@@ -5,27 +5,38 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.List;
 
 public class AdminPage extends AppCompatActivity {
 
-    private FirebaseAuth mAuth;
-    private DatabaseReference dB;
+    FirebaseAuth mAuth;
+    DatabaseReference dB;
+    List<Complaint> complaints;
+    ListView listViewComplaints;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adminpage);
         mAuth = FirebaseAuth.getInstance();
-        //dB = FirebaseDatabase.getInstance().getReference("UID");
+        dB = FirebaseDatabase.getInstance().getReference("log");
         Button adminLogoutButton = (Button) findViewById(R.id.adminLogoutButton);
-
+        listViewComplaints = (ListView) findViewById(R.id.list_of_complaints);
 
         adminLogoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,13 +44,45 @@ public class AdminPage extends AppCompatActivity {
                 logOut(view);
             }
         });
+
+    /*listen for long click on items   listViewComplaints.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //Complaint complaint = complaints.get(i);
+                showUpdateDeleteDialog(complaint.getId(), complaint.getProductName());
+                return true;
+            }
+        }); */
     }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        dB.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                complaints.clear();
+                for (DataSnapshot post: snapshot.getChildren()) {
+                    Complaint complaint = post.getValue(Complaint.class);
+                    complaints.add(complaint);
+                }
+                ComplaintList productsAdapter = new ComplaintList(AdminPage.this,complaints);
+                listViewComplaints.setAdapter(productsAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+
     public void logOut(View view){
         mAuth.signOut();
         finish();
     }
 
-   /* public void showMealEntry(final String productId, String productName) {
+    /*public void showMealEntry(final String productId, String productName) {
 
         AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
         LayoutInflater inflater = getLayoutInflater();
@@ -67,12 +110,12 @@ public class AdminPage extends AppCompatActivity {
             }
         });
 
-        buttonDelete.setOnClickListener(new View.OnClickListener() {
+        buttonDelete.setOnClickListener(new View.OnClickListener    () {
             @Override
             public void onClick(View view) {
                 deleteProduct(productId);
                 b.dismiss();
             }
         });
-    }*/
+    } */
 }
