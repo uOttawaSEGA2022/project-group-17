9e0733b35acf3c4a8d9a8ed6c14a8359efdc136c
Nathan Le <nle056@uottawa.ca>
2022-11-06 16:30:24 -0500

    upload profile picture

--- app/src/main/java/com/example/kachow_now/AdminPage.java
+++ app/src/main/java/com/example/kachow_now/AdminPage.java
@@ -1,8 +1,12 @@
 package com.example.kachow_now;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
@@ -34,4 +38,41 @@ public class AdminPage extends AppCompatActivity {
         mAuth.signOut();
         finish();
     }
+
+   /* public void showMealEntry(final String productId, String productName) {
+
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        final View dialogView = inflater.inflate(R.layout.CookProfileClientSide, null);
+        dialogBuilder.setView(dialogView);
+
+        final EditText editTextName = (EditText) dialogView.findViewById(R.id.editTextName);
+        final EditText editTextPrice  = (EditText) dialogView.findViewById(R.id.editTextPrice);
+        final Button buttonUpdate = (Button) dialogView.findViewById(R.id.buttonUpdateProduct);
+        final Button buttonDelete = (Button) dialogView.findViewById(R.id.buttonDeleteProduct);
+
+        dialogBuilder.setTitle(productName);
+        final AlertDialog b = dialogBuilder.create();
+        b.show();
+
+        buttonUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = editTextName.getText().toString().trim();
+                double price = Double.parseDouble(String.valueOf(editTextPrice.getText().toString()));
+                if (!TextUtils.isEmpty(name)) {
+                    updateProduct(productId, name, price);
+                    b.dismiss();
+                }
+            }
+        });
+
+        buttonDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                deleteProduct(productId);
+                b.dismiss();
+            }
+        });
+    }*/
 }
