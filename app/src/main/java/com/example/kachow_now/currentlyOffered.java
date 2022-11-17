0d5f97284cc571395fd1b7ac4e07d8170c8892a9
KevinDang1 <kdang038@uottawa.ca>
2022-11-17 18:12:08 -0500

    offered button code

--- app/src/main/java/com/example/kachow_now/currentlyOffered.java
+++ app/src/main/java/com/example/kachow_now/currentlyOffered.java
@@ -1,11 +1,14 @@
 package com.example.kachow_now;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -39,12 +42,55 @@ public class currentlyOffered extends AppCompatActivity {
 
         meals = new ArrayList<Meal>();
 
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+
+
         listViewMeals.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                // TODO WHAT DO WE DO HERE???
+                final View dialogView = inflater.inflate(R.layout.cook_offered_dialog, null);
+                dialogBuilder.setView(dialogView);
+                Button toggleOffer = dialogView.findViewById(R.id.toggleoffer);
+                Button deleteMeal = dialogView.findViewById(R.id.deletemeal);
+                final AlertDialog b = dialogBuilder.create();
+
+                toggleOffer.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        dB.child(String.valueOf(meals.get(position))).addValueEventListener( new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                boolean isOffered = snapshot.child("isOffered").getValue(boolean.class);
+                                if (isOffered){
+                                    dB.child(String.valueOf(meals.get(position))).child("isOffered").removeValue();
+                                    dB.child(String.valueOf(meals.get(position))).child("isOffered").setValue(false);
+                                }else{
+                                    dB.child(String.valueOf(meals.get(position))).child("isOffered").removeValue();
+                                    dB.child(String.valueOf(meals.get(position))).child("isOffered").setValue(true);
+                                }
+
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+
+                            }
+                        });
+                    }
+                });
+                deleteMeal.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(currentlyOffered.this, "Deleted Meal", Toast.LENGTH_LONG).show();
+                        dB.child(String.valueOf(meals.get(position))).removeValue();
+                    }
+                });
+                b.show();
                 return true;
             }
+
         });
 
 
