bdd9d1845ed515c542195d47751fbe98b5afe797
SivaS2003 <91794547+SivaS2003@users.noreply.github.com>
2022-11-17 19:31:17 -0500

    wrote 3 test cases for salad

--- app/src/test/java/com/example/kachow_now/MealTest.java
+++ app/src/test/java/com/example/kachow_now/MealTest.java
@@ -1,32 +1,38 @@
 package com.example.kachow_now;
 
-
 import static org.junit.Assert.*;
-import java.util.ArrayList;
-
+import java.util.*;
 import org.junit.Test;
 
 public class MealTest {
 
     @Test
     public void getName() {
-        ArrayList<String> ingredients = new ArrayList<String>();
-        ArrayList<String> allergens = new ArrayList<String>();
-
-        Meal daal = new Meal("daal", "yummy yummy good good", "dinner", 32.0, "south east asian", ingredients, allergens, 2, 120);
-        assertEquals("daal", daal.getName());
     }
 
     @Test
     public void setName() {
-        Meal rightinfrontofmysalad = new Meal();
+        ArrayList<String> ingredients = new ArrayList<String>();
+        ingredients.add("lettuce");
+        Meal salad = new Meal("Salad", "well-dressed salad", "Lunch", 5.00, "Mediterranean", ingredients, null, 2.00, 200.00 );
+        salad.setName("Mediterranean Salad");
+        assertEquals( "Mediterranean Salad", salad.getName());
     }
 
     @Test
     public void getDescription() {
+        ArrayList<String> ingredients = new ArrayList<String>();
+        ingredients.add("lettuce");
+        Meal salad = new Meal("Salad", "well-dressed salad", "Lunch", 5.00, "Mediterranean", ingredients, null, 2.00, 200.00 );
+        assertEquals( "well-dressed salad", salad.getDescription());
     }
 
     @Test
     public void setDescription() {
+        ArrayList<String> ingredients = new ArrayList<String>();
+        ingredients.add("lettuce");
+        Meal salad = new Meal("Salad", "well-dressed salad", "Lunch", 5.00, "Mediterranean", ingredients, null, 2.00, 200.00 );
+        salad.setDescription("VERY well-dressed salad");
+        assertEquals( "VERY well-dressed salad", salad.getDescription());
     }
 }
