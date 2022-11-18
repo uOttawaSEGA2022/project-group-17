b4f2741406c5540a86d2e0465be27081a0d92f1d
sami-hssn <113709975+sami-hssn@users.noreply.github.com>
2022-11-17 19:29:40 -0500

    unit test

--- app/src/test/java/com/example/kachow_now/MealTest.java
+++ app/src/test/java/com/example/kachow_now/MealTest.java
@@ -1,6 +1,8 @@
 package com.example.kachow_now;
 
+
 import static org.junit.Assert.*;
+import java.util.ArrayList;
 
 import org.junit.Test;
 
@@ -8,11 +10,16 @@ public class MealTest {
 
     @Test
     public void getName() {
+        ArrayList<String> ingredients = new ArrayList<String>();
+        ArrayList<String> allergens = new ArrayList<String>();
+
+        Meal daal = new Meal("daal", "yummy yummy good good", "dinner", 32.0, "south east asian", ingredients, allergens, 2, 120);
+        assertEquals("daal", daal.getName());
     }
 
     @Test
     public void setName() {
-        Meal rightinfrontofmysalad = new Meal()
+        Meal rightinfrontofmysalad = new Meal();
     }
 
     @Test
