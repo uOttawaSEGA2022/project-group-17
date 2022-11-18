6a6edad12804326749211f74e8b928f21e496542
SivaS2003 <91794547+SivaS2003@users.noreply.github.com>
2022-11-17 19:15:36 -0500

    created 4 test cases for meal

---
+++ app/src/test/java/com/example/kachow_now/MealTest.java
@@ -0,0 +1,24 @@
+package com.example.kachow_now;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MealTest {
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void setName() {
+    }
+
+    @Test
+    public void getDescription() {
+    }
+
+    @Test
+    public void setDescription() {
+    }
+}
