b730387abd35059b879ecc00efeaf80b6e7efc0e
Nathan Le <nle056@uottawa.ca>
2022-11-26 17:14:05 -0500

    request

--- app/src/main/java/com/example/kachow_now/Request.java
+++ app/src/main/java/com/example/kachow_now/Request.java
@@ -1,2 +1,20 @@
-package com.example.kachow_now;public class Request {
+package com.example.kachow_now;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Request {
+
+    private String clientId;
+    private String cookId;
+    private long currentTime;
+    private ArrayList orders;
+
+    public Request(){
+        this.orders = new ArrayList();
+    }
+
+    public void orderMeal(Meal meal){
+        orders.add(meal);
+    }
 }
