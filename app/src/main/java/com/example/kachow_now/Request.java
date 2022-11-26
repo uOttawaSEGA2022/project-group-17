22829cd6b5559d97fe57794528040c3096053990
Nathan Le <nle056@uottawa.ca>
2022-11-26 17:26:38 -0500

    Request

--- app/src/main/java/com/example/kachow_now/Request.java
+++ app/src/main/java/com/example/kachow_now/Request.java
@@ -8,13 +8,25 @@ public class Request {
     private String clientId;
     private String cookId;
     private long currentTime;
-    private ArrayList orders;
+    private ArrayList<Pair<String,Long>> orders;
+
+    public class Pair<String, Long> {
+        private String left;
+        private Long right;
+
+        public Pair(String mealName,Long currentTime){
+            this.left = mealName;
+            this.right = currentTime;
+        }
+    }
 
     public Request(){
-        this.orders = new ArrayList();
+        this.orders = new ArrayList<>();
     }
 
     public void orderMeal(Meal meal){
-        orders.add(meal);
+        orders.add(new Pair(meal.getName(),System.currentTimeMillis()));
+
     }
 }
+
