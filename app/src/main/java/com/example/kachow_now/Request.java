f62d32e8bf6212919bd987746b51efabe8419362
Nathan Le <nle056@uottawa.ca>
2022-11-26 23:59:02 -0500

    cookOrders not finished

--- app/src/main/java/com/example/kachow_now/Request.java
+++ app/src/main/java/com/example/kachow_now/Request.java
@@ -9,21 +9,54 @@ public class Request {
     private String cookId;
     private String currentTime;
     private ArrayList<String> orders;
+    private String state;
 
     public Request(ArrayList<String> mealList, String cookId, String clientId){
         this.orders = mealList;
         this.cookId = cookId;
         this.clientId = clientId;
         this.currentTime = Long.toString(System.currentTimeMillis());
+        this.state = "pending";
+    }
+    public Request(){
 
     }
 
-    public void makeOrder(Meal meal){
+    public void add(Meal meal){
         orders.add(meal.getName());
 
     }
     public void removeMeal(int i){
         orders.remove(i);
+    }
+
+    public void setState(String state){
+        this.state = state;
+    }
+    public String getState(){
+        return this.state;
+    }
+
+    public void setCookId(String cookId){
+        this.cookId = cookId;
+    }
+    public String getCookId(){
+        return this.cookId;
+    }
+    public void setClientId(String clientId){
+        this.clientId = clientId;
+    }
+    public String getClientId(){
+        return this.clientId;
+    }
+    public String getCurrentTime(){
+        return this.currentTime;
+    }
+    public void setCurrentTime(String currentTime){
+        this.currentTime = currentTime;
+    }
+    public void setOrders(ArrayList<String> orders){
+        this.orders = orders;
     }
 }
 
