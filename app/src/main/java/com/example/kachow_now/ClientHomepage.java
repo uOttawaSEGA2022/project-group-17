1f053deb7506101947083588b5da4eef2dca2d55
Aaditya Shah <ashah203@uottawa.ca>
2022-12-02 21:14:37 -0500

    Search works now, RATING WORKS NOW
    Signed-off-by: Aaditya Shah <ashah203@uottawa.ca>

--- app/src/main/java/com/example/kachow_now/ClientHomepage.java
+++ app/src/main/java/com/example/kachow_now/ClientHomepage.java
@@ -1,10 +1,15 @@
 package com.example.kachow_now;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -15,6 +20,7 @@ import com.google.firebase.database.GenericTypeIndicator;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -42,7 +48,89 @@ public class ClientHomepage extends AppCompatActivity {
 
         //TODO if previous order has been accepted and ordered by this client, do the rate the
         // meal menu, This is just an idea though
-        rateMealDB = FirebaseDatabase.getInstance().getReference("MEALS");
+        rateMealDB = FirebaseDatabase.getInstance().getReference("CLIENTLOG")
+                .child(mAuth.getCurrentUser().getUid());
+
+        rateMealDB.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot s : snapshot.getChildren()) {
+                    if (s.child("accepted").getValue(boolean.class) == null) {
+
+                        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(ClientHomepage.this);
+                        LayoutInflater inflater = getLayoutInflater();
+                        final View dialogView = inflater.inflate(R.layout.rate_cook_dialog, null);
+
+                        TextView labelRating = dialogView.findViewById(R.id.labelRating1);
+                        labelRating.setText("Rate your last meal");
+                        EditText ratingInput = dialogView.findViewById(R.id.ratingCook);
+                        ratingInput.setHint("Enter a rating from 1 to 5");
+                        Button rateCook = dialogView.findViewById(R.id.rateCook);
+
+                        dialogBuilder.setView(dialogView);
+                        final AlertDialog b = dialogBuilder.create();
+                        b.show();
+
+                        rateCook.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                String c = ratingInput.getText().toString();
+                                try {
+                                    double userRating = Double.parseDouble(c);
+
+                                    if ((userRating <= 0) || (userRating > 5)) {
+                                        throw new IllegalArgumentException();
+                                    }
+
+                                    DatabaseReference mealDb = FirebaseDatabase.getInstance().
+                                            getReference("MEALS")
+                                            .child(Objects.requireNonNull(s.child("cookId") //child(cookID)
+                                                    .getValue(String.class)))
+                                            .child(Objects.requireNonNull(s.child("orders")//child(mealName)
+                                                    .child("0").getValue(String.class)));
+                                    mealDb.addListenerForSingleValueEvent(new ValueEventListener() {
+                                        @Override
+                                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                            Double dbRating = snapshot.child("rating").getValue(Double.class);
+                                            mealDb.child("rating").removeValue();
+                                            if (dbRating == 0) {
+                                                mealDb.child("rating").setValue(userRating);
+                                            } else {
+                                                mealDb.child("rating").setValue((userRating + dbRating) / 2);
+                                            }
+                                        }
+
+                                        @Override
+                                        public void onCancelled(@NonNull DatabaseError error) {
+
+                                        }
+                                    });
+
+
+                                    b.dismiss();
+                                } catch (NullPointerException e) {
+                                    Toast.makeText(ClientHomepage.this,
+                                            "Please enter a rating", Toast.LENGTH_LONG).show();
+                                } catch (NumberFormatException e) {
+                                    Toast.makeText(ClientHomepage.this,
+                                            "Please enter a number", Toast.LENGTH_LONG).show();
+                                } catch (IllegalArgumentException e) {
+                                    Toast.makeText(ClientHomepage.this,
+                                            "Please enter a number between 1 and 5", Toast.LENGTH_LONG).show();
+                                }
+
+                            }
+                        });
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
         // scroll through all info and then give them the popup
 
         SearchView s = findViewById(R.id.Search);
--- app/src/main/java/com/example/kachow_now/CookProfileClientSide.java
+++ app/src/main/java/com/example/kachow_now/CookProfileClientSide.java
@@ -7,11 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -19,6 +15,9 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class CookProfileClientSide extends AppCompatActivity {
     private FirebaseAuth mAuth;
@@ -85,7 +84,7 @@ public class CookProfileClientSide extends AppCompatActivity {
                 final View dialogView = inflater.inflate(R.layout.rate_cook_dialog, null);
 
                 EditText ratingInput = dialogView.findViewById(R.id.ratingCook);
-                Button rateCook = dialogView.findViewById(R.id.ratingCook);
+                Button rateCook = dialogView.findViewById(R.id.rateCook);
 
                 dialogBuilder.setView(dialogView);
                 final AlertDialog b = dialogBuilder.create();
