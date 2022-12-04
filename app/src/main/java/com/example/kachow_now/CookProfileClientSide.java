67918b33e19fef00b30d0b28411354c84b40911a
sami-hssn <113709975+sami-hssn@users.noreply.github.com>
2022-12-03 19:43:34 -0500

    removed todo

--- app/src/main/java/com/example/kachow_now/CookProfileClientSide.java
+++ app/src/main/java/com/example/kachow_now/CookProfileClientSide.java
@@ -84,39 +84,7 @@ public class CookProfileClientSide extends AppCompatActivity {
 
         } catch (Exception ignored) {
         }
-        
-        /*Cook tmpCook = new Cook();
-        dB.child(cUID).addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot cookSnapshot) {
-                Cook tmpCook = new Cook();
-                tmpCook.setUID(cookSnapshot.child("uid").getValue(String.class));
-                tmpCook.setAddress(cookSnapshot.child("address").getValue(String.class));
 
-                tmpCook.setBank(cookSnapshot.child("bank").getValue(new GenericTypeIndicator<ArrayList<Long>>() {
-                }));
-
-                tmpCook.setDaySus(cookSnapshot.child("daySus").getValue(Integer.class));
-                tmpCook.setDescription(cookSnapshot.child("description").getValue(String.class));
-                tmpCook.setEmail(cookSnapshot.child("email").getValue(String.class));
-                tmpCook.setFirstName(cookSnapshot.child("firstName").getValue(String.class));
-                tmpCook.setIsBanned(cookSnapshot.child("isBanned").getValue(boolean.class));
-                tmpCook.setIsSuspended(cookSnapshot.child("isSuspended").getValue(boolean.class));
-                tmpCook.setLastName(cookSnapshot.child("lastName").getValue(String.class));
-                tmpCook.setPassword(cookSnapshot.child("password").getValue(String.class));
-                tmpCook.setPhoneNumber(cookSnapshot.child("phoneNumber").getValue(long.class));
-                tmpCook.setPostalCode(cookSnapshot.child("postalCode").getValue(String.class));
-                tmpCook.setRating(cookSnapshot.child("rating").getValue(Integer.class));
-                tmpCook.setRole(cookSnapshot.child("role").getValue(String.class));
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });*/
-
-        //TODO DO THIS AFTER DOING THE COOK SIDE FIRST
 
         Button reportButton = (Button) findViewById(R.id.ReportCookProfileClient);
         reportButton.setOnClickListener(new View.OnClickListener() {
